847497d90dbced62529543c69c35fabab604d38f
saintvrai <ishanovairat@gmail.com>
2023-09-10 18:01:29 +0300

    init http server and endpoints

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"auth"
+	"auth/pkg/handler"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(auth.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error running http server %s", err.Error())
 	}
 }
